93541fc3e2d692c097540367221cff1ef181dc51
Michael Boateng <38674372+moaboateng@users.noreply.github.com>
2025-02-22 14:42:14 +0000

    Bug Fixes
    Bug Fixes

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit67c7a1b3ab483bb511f14785bd808fa7
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit67c7a1b3ab483bb511f14785bd808fa7', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit67c7a1b3ab483bb511f14785bd808fa7', 'loadClassLoader'));
